a7e7c22023468b106387db2bf21b80e7f29860f7
WEI <111346524+Arrozex@users.noreply.github.com>
2025-06-06 22:53:43 +0800

    Update page.tsx

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,12 +1,7 @@
 import dynamic from 'next/dynamic'
-import { Suspense } from 'react'
 
-const ResultContent = dynamic(() => import('./ResultContent'), { ssr: false })
+const ResultContent = dynamic(() => import('./ResultContent'))
 
-export default function Page(): JSX.Element {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <ResultContent />
-    </Suspense>
-  )
+export default function Page() {
+  return <ResultContent />
 }
