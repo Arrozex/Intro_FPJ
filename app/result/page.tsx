483dda24d7f4186becd6d4e06ca8d9f26ae1239d
WEI <111346524+Arrozex@users.noreply.github.com>
2025-06-06 22:51:35 +0800

    Update page.tsx

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,7 +1,6 @@
 import dynamic from 'next/dynamic'
 import { Suspense } from 'react'
 
-// 動態載入 client component
 const ResultContent = dynamic(() => import('./ResultContent'), { ssr: false })
 
 export default function Page(): JSX.Element {
